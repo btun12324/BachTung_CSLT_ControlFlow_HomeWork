6c7c4ee65cd46d2b1f42242984f21d7a7bf6cada
btun12324 <bachtung303207@gmail.com>
2026-09-10 15:36:35 +0700

    upload EX01

--- EX01/Program.cs
+++ EX01/Program.cs
@@ -1,3 +1,40 @@
 ﻿using System;
 
-namespace 
+namespace ControlFlow_HomeWork
+{
+    internal class EX01
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Tuổi: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Giờ chiếu: ");
+            int time = int.Parse(Console.ReadLine());
+
+            int giave = 0;
+
+            if (age < 12 || age > 60)
+            {
+                giave = 50000;
+            }
+            else
+            {
+                if (time < 17)
+                {
+                    giave = 80000;
+                }
+                else
+                {
+                    giave = 110000;
+                }
+            }
+
+            Console.WriteLine("Giá vé của bạn là: " + giave + " VNĐ");
+
+            Console.ReadKey();
+        }
+    }
+}
