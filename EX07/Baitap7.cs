482c3698186cb44e90aab3328f6642609c4c9c0f
btun12324 <bachtung303207@gmail.com>
2026-09-14 13:17:49 +0700

    upload EX07

---
+++ EX07/Baitap7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSLT_ControlFlow_HomeWork
+{
+    internal class EX07
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Cân nặng = ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Chiều cao = ");
+            double height = double.Parse(Console.ReadLine());
+
+            double BMI = weight / (height * height);
+            BMI = Math.Round(BMI, 2);
+
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("BMI: " + BMI + " - Đánh giá : Gầy - Nên bổ sung dinh dưỡng.");
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                Console.WriteLine("BMI: " + BMI + " - Đánh giá : Cân đối - tiếp tục duy trì.");
+            }
+            else if (BMI >= 25 && BMI <30)
+            {
+                Console.WriteLine("BMI: " + BMI + " - Đánh giá : Thừa cân - Nên tăng cường luyện tập.");
+            }
+            else
+            {
+                Console.WriteLine("BMI: " + BMI + " - Đánh giá : Béo phì - Cần sự tư vấn từ bác sĩ.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
